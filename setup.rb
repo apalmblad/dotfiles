3147f7dee1e12955859bc4cb48eda42f631381df
Adam Palmblad <adam.palmblad@teampages.com>
2010-01-29 20:52:29 -0800

    Initial commit of dotfiles.

---
+++ setup.rb
@@ -0,0 +1,11 @@
+#!/usr/bin/ruby
+require 'pathname'
+dh = Dir.open( "." )
+dir = Pathname.new( __FILE__ ).realpath.dirname
+while file = dh.read
+  next if file =~ /^\.+$/
+  next if file == __FILE__
+  next if file == '.git'
+  puts "Linking #{dir}/#{file} ~/.#{file}"
+  `ln -i -s #{dir}/#{file} ~/.#{file}`
+end
