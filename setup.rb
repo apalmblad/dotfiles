a216fe14158f0b6bf0ac3d35cd2bf95b054e0dcf
Adam Palmblad <adam.palmblad@teampages.com>
2010-03-09 12:06:17 -0800

    No need to link the README file.

--- setup.rb
+++ setup.rb
@@ -5,6 +5,7 @@ dir = Pathname.new( __FILE__ ).realpath.dirname
 while file = dh.read
   next if file =~ /^\.+$/
   next if file == __FILE__
+  next if file == 'README'
   next if file == '.git'
   puts "Linking #{dir}/#{file} ~/.#{file}"
   `ln -i -s #{dir}/#{file} ~/.#{file}`
